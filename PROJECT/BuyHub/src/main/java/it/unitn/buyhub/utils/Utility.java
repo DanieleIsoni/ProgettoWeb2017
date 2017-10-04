9dfe1357258c1e381232e270135c09184b69ba23
Massimo Girondi <massimo@girondi.net>
2017-10-04 21:38:09 +0200

    Working saveFile method

--- PROJECT/BuyHub/src/main/java/it/unitn/buyhub/utils/Utility.java
+++ PROJECT/BuyHub/src/main/java/it/unitn/buyhub/utils/Utility.java
@@ -5,6 +5,15 @@
  */
 package it.unitn.buyhub.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.jstl.fmt.LocaleSupport;
@@ -33,6 +42,41 @@ public class Utility {
  
      static public enum CAPABILITY{INVALID,USER,SHOP,ADMIN};
      
+     
+     
+     
+     public static String saveFile(InputStream is,String name)
+     {
+        
+         try {
+             String uploads=PropertyHandler.getInstance().getValue("uploadedContentFolder");
+             File uploadsfolder=new File(uploads);
+             // if the directory does not exist, create it
+             if (!uploadsfolder.exists()) {
+                 Log.info("The uploadedContentFolder is not present. Creating it to "+uploadsfolder.getAbsolutePath());
+                 
+                 boolean result = false;
+                 try{
+                     uploadsfolder.mkdir();
+                     result = true;
+                 }
+                 catch(SecurityException se){
+                     Log.error("Error creating the directory: "+se.getMessage());
+                 }
+             }
+             
+             File file = new File(uploads,name);
+             file.createNewFile();
+             
+             Files.copy(is, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+             Log.info("File saved to "+file.getAbsolutePath());
+             return "0";
+         } catch (IOException ex) {
+           Log.error("Error saving file: "+ex.getMessage());
+          }
+         
+         return name;
+     }
      
 }
 
